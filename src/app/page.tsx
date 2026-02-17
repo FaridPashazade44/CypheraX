bb2c2159de02e398494bb2003f72f296b89b70cf
FaridPashazade44 <fabdurahmanov@std.beu.edu.az>
2026-02-17 22:00:17 +0400

    Create landing page component

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Welcome to CypheraX</h1>
+            <p>Your go-to app for secure communications.</p>
+        </div>
+    );
+};
+
+export default LandingPage;
