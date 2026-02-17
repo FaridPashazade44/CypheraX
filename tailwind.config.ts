b2f9621edca7b2ddd4cfcbbf374f96401674febc
FaridPashazade44 <fabdurahmanov@std.beu.edu.az>
2026-02-17 21:57:15 +0400

    Create tailwind.config.ts with dark-blue theme configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#1e3a8a',
+      },
+    },
+  },
+  plugins: [],
+};
